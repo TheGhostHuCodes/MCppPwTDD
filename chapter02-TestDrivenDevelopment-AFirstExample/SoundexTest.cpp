78bde59fa52392e297dea324d92c6e2a3a2d476c
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-01-08 23:11:04 -0800

    Add and change tests for zero padding. Refactor zeroPad method out of encode.

--- chapter02-TestDrivenDevelopment-AFirstExample/SoundexTest.cpp
+++ chapter02-TestDrivenDevelopment-AFirstExample/SoundexTest.cpp
@@ -3,7 +3,12 @@
 class Soundex {
 public:
     std::string encode(const std::string& word) const {
-        return word;
+        return zeroPad(word);
+    }
+
+private:
+    std::string zeroPad(const std::string& word) const {
+        return word + "000";
     }
 };
 
@@ -15,5 +20,13 @@ TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
 
     auto encoded = soundex.encode("A");
 
-    ASSERT_THAT(encoded, Eq("A"));
+    ASSERT_THAT(encoded, Eq("A000"));
+}
+
+TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
+    Soundex soundex;
+
+    auto encoded = soundex.encode("I");
+
+    ASSERT_THAT(encoded, Eq("I000"));
 }
