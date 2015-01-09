3bd37ded70fc083cd085640d16930563c85346ba
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-01-08 22:59:12 -0800

    Refactor encode for function and test for readability.

--- chapter02-TestDrivenDevelopment-AFirstExample/SoundexTest.cpp
+++ chapter02-TestDrivenDevelopment-AFirstExample/SoundexTest.cpp
@@ -3,16 +3,17 @@
 class Soundex {
 public:
     std::string encode(const std::string& word) const {
-        return "A";
+        return word;
     }
 };
 
 #include "gmock/gmock.h"
+using ::testing::Eq;
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
     Soundex soundex;
 
     auto encoded = soundex.encode("A");
 
-    ASSERT_THAT(encoded, testing::Eq("A"));
+    ASSERT_THAT(encoded, Eq("A"));
 }
