daeb93de1c8cbb137c86d9fc2b0756ec63baa0c4
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-01-10 10:54:03 -0800

    Refactor private methods for zero padding, returning the head of a string, and encoding digits.

--- chapter02-TestDrivenDevelopment-AFirstExample/Soundex.h
+++ chapter02-TestDrivenDevelopment-AFirstExample/Soundex.h
@@ -6,12 +6,28 @@
 class Soundex {
 public:
     std::string encode(const std::string& word) const {
-        return zeroPad(word);
+        return zeroPad(head(word) + encodedDigits(word));
     }
 
 private:
+    std::string head(const std::string& word) const {
+        return word.substr(0, 1);
+    }
+
+    std::string encodedDigits(const std::string& word) const {
+            if (word.length() > 1) return encodedDigit();
+            else return "";
+    }
+
+    std::string encodedDigit() const {
+        return "1";
+    }
+
+    static const size_t maxCodeLength{4};
+
     std::string zeroPad(const std::string& word) const {
-        return word + "000";
+        auto zerosNeeded = maxCodeLength - word.length();
+        return word + std::string(zerosNeeded, '0');
     }
 };
 #endif
