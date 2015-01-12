821c6c0ce38fa270a496b9265b0e70012de14ea5
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-01-11 22:24:55 -0800

    Add test and code to ensure encoding combines duplicate codes when the second letter encoding is a duplicate of the first letter encoding.

--- chapter02-TestDrivenDevelopment-AFirstExample/Soundex.h
+++ chapter02-TestDrivenDevelopment-AFirstExample/Soundex.h
@@ -7,7 +7,7 @@
 class Soundex {
 public:
     std::string encode(const std::string& word) const {
-        return zeroPad(upperFront(head(word)) + encodedDigits(tail(word)));
+        return zeroPad(upperFront(head(word)) + tail(encodedDigits(word)));
     }
 
     std::string encodedDigit(char letter) const {
@@ -43,15 +43,19 @@ private:
     }
 
     bool isComplete(const std::string& encoding) const {
-        return encoding.length() == maxCodeLength - 1;
+        return encoding.length() == maxCodeLength;
     }
 
     const std::string NotADigit {"*"};
 
     std::string encodedDigits(const std::string& word) const {
         std::string encoding;
-        for (auto letter: word) {
+
+        encoding += encodedDigit(word.front());
+
+        for (auto letter: tail(word)) {
             if (isComplete(encoding)) break;
+
             auto digit = encodedDigit(letter);
             if (digit != NotADigit && digit != lastDigit(encoding)) {
                 encoding += encodedDigit(letter);
--- chapter02-TestDrivenDevelopment-AFirstExample/SoundexTest.cpp
+++ chapter02-TestDrivenDevelopment-AFirstExample/SoundexTest.cpp
@@ -51,3 +51,7 @@ TEST_F(SoundexEncoding, UppercasesFirstLetter) {
 TEST_F(SoundexEncoding, IgnoreCaseWhenEncodingConsonants) {
     ASSERT_THAT(soundex.encode("BCDL"), Eq(soundex.encode("bcdl")));
 }
+
+TEST_F(SoundexEncoding, CombinesDuplicateCodesWhen2ndLetterDuplicates1st) {
+    ASSERT_THAT(soundex.encode("Bbcd"), Eq("B230"));
+}
