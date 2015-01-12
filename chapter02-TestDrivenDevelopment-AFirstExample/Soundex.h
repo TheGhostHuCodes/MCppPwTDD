327e61c06eb2678b64786190fb3e4aceabcae5f0
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-01-11 21:19:01 -0800

    Add test and code to limit encoding length to four characters.

--- chapter02-TestDrivenDevelopment-AFirstExample/Soundex.h
+++ chapter02-TestDrivenDevelopment-AFirstExample/Soundex.h
@@ -21,8 +21,15 @@ private:
 
     std::string encodedDigits(const std::string& word) const {
         std::string encoding;
-        for (auto letter: word) encoding += encodedDigit(letter);
+        for (auto letter: word) {
+            if (isComplete(encoding)) break;
+            else encoding += encodedDigit(letter);
+        }
         return encoding;
+    }
+
+    bool isComplete(const std::string& encoding) const {
+        return encoding.length() == maxCodeLength - 1;
     }
 
     std::string encodedDigit(char letter) const {
