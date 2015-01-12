18412a454577ba2f6aa208fd0e7b3981c94803fa
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-01-11 16:54:27 -0800

    Add character encoding map and change test to exercise new character in the map.

--- chapter02-TestDrivenDevelopment-AFirstExample/SoundexTest.cpp
+++ chapter02-TestDrivenDevelopment-AFirstExample/SoundexTest.cpp
@@ -17,5 +17,5 @@ TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
 }
 
 TEST_F(SoundexEncoding, ReplaceConsonantsWithAppropriateDigits) {
-    ASSERT_THAT(soundex.encode("Ab"), Eq("A100"));
+    ASSERT_THAT(soundex.encode("Ax"), Eq("A200"));
 }
