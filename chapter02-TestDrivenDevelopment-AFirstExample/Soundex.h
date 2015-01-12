b01c1070c18c3e725e584c1aa68f3ba146d75460
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-01-11 21:32:58 -0800

    Add test and code to combine duplicate encoding values when they are next to each other.
    Make encodedDigit public to simplify testing.

--- chapter02-TestDrivenDevelopment-AFirstExample/Soundex.h
+++ chapter02-TestDrivenDevelopment-AFirstExample/Soundex.h
@@ -10,28 +10,6 @@ public:
         return zeroPad(head(word) + encodedDigits(tail(word)));
     }
 
-private:
-    std::string head(const std::string& word) const {
-        return word.substr(0, 1);
-    }
-
-    std::string tail(const std::string& word) const {
-        return word.substr(1);
-    }
-
-    std::string encodedDigits(const std::string& word) const {
-        std::string encoding;
-        for (auto letter: word) {
-            if (isComplete(encoding)) break;
-            else encoding += encodedDigit(letter);
-        }
-        return encoding;
-    }
-
-    bool isComplete(const std::string& encoding) const {
-        return encoding.length() == maxCodeLength - 1;
-    }
-
     std::string encodedDigit(char letter) const {
         const std::unordered_map<char, std::string> encodings {
             {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"}
@@ -44,6 +22,35 @@ private:
         };
         auto it = encodings.find(letter);
         return it == encodings.end() ? "" : it->second;
+    }
+
+private:
+    std::string head(const std::string& word) const {
+        return word.substr(0, 1);
+    }
+
+    std::string tail(const std::string& word) const {
+        return word.substr(1);
+    }
+
+    bool isComplete(const std::string& encoding) const {
+        return encoding.length() == maxCodeLength - 1;
+    }
+
+    std::string encodedDigits(const std::string& word) const {
+        std::string encoding;
+        for (auto letter: word) {
+            if (isComplete(encoding)) break;
+            else if (encodedDigit(letter) != lastDigit(encoding)) {
+                encoding += encodedDigit(letter);
+            }
+        }
+        return encoding;
+    }
+
+    std::string lastDigit(const std::string& encoding) const {
+        if (encoding.empty()) return "";
+        else return std::string(1, encoding.back());
     }
 
     static const size_t maxCodeLength{4};
